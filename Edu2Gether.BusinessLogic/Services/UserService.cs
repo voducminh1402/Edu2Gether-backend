3a8a92da6199489bc121cc00ec49c6ac03c1a354
voducminh <voducminh140201@gmail.com>
2022-11-09 08:59:15 +0700

    [MinhVD] - hot fix add user services

--- Edu2Gether.BusinessLogic/Services/UserService.cs
+++ Edu2Gether.BusinessLogic/Services/UserService.cs
@@ -1,22 +1,57 @@
 
+using AutoMapper;
+using Edu2Gether.BusinessLogic.Commons;
+using Edu2Gether.BusinessLogic.ViewModels;
 using Edu2Gether.DataAccess.Repositories;
+using System.Linq;
 
 namespace Edu2Gether.BusinessLogic.Services 
 {
 
     public interface IUserService {
-        
+        public UserResponseModel ChangeRoleToMentor(string menteeId);
     }
 
     public class UserService : IUserService {
 
-      private readonly IUserRepository _userRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IMenteeRepository _menteeRepository;
 
-        public UserService(IUserRepository userRepository)
+        public UserService(IUserRepository userRepository, IMenteeRepository menteeRepository, IMapper mapper)
         {
+            _menteeRepository = menteeRepository;
+            _mapper = mapper;
             _userRepository = userRepository;
         }
 
+        public UserResponseModel ChangeRoleToMentor(string menteeId)
+        {
+            var mentee = _menteeRepository.Get().Where(x => x.Id.Equals(menteeId)).FirstOrDefault();
+
+            if (mentee != null)
+            {
+                var user = _userRepository.Get().Where(x => x.Id.Equals(menteeId)).FirstOrDefault();
+
+                if (user.RoleId != (int) UserRole.Mentee)
+                {
+                    return null;
+                }
+
+                user.RoleId = (int)UserRole.Mentor;
+                var userUpdated = _userRepository.Update(user);
+                _userRepository.Save();
+
+                if (userUpdated.RoleId == (int)UserRole.Mentor)
+                {
+                    _menteeRepository.Delete(mentee);
+                    _menteeRepository.Save();
+                }
+
+                return _mapper.Map<UserResponseModel>(userUpdated);
+            }
+            return null;
+        }
     }
 
 }
---
+++ Edu2Gether.Presentation/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Edu2Gether.BusinessLogic.Services;
+using Edu2Gether.BusinessLogic.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edu2Gether.Presentation.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v1/users")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [MapToApiVersion("1")]
+        [HttpPost("{userId}/change-role")]
+        public ActionResult<UserResponseModel> GetUserByWallet(string userId)
+        {
+            UserResponseModel user = _userService.ChangeRoleToMentor(userId);
+
+            if (user == null)
+            {
+                return NotFound("Can't change role of user!");
+            }
+            return Ok(user);
+        }
+    }
+}
